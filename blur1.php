d17b2ceb2e0e700b0b615126c4c89a8d348b4e98
M Luana <lu.pinheiro.ern@gmail.com>
2024-05-23 20:38:15 -0300

    first commit

---
+++ blur1.php
@@ -0,0 +1,52 @@
+<?php include('config.php'); ?>
+<!DOCTYPE html>
+<head>
+ <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>NutriAcao</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" href="style/style.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+<div class="navbar">
+    <div class="logo-container">
+      <img src="assets/logo.jpeg" alt="Logo" class="logo-img">
+      <span class="company-name"><strong>Nutri</strong>Ação</span>
+    </div>
+</div>
+<br>
+<div id="exclusiv">
+    <h2><img src="assets/logo.jpeg" alt="Logo" class="logo-img" styles="border-radius: 100%;">Quer ter acesso a conteúdos exclusivos?</h2>
+    <p>Cadastre sua conta e desfrute de todos nossos materiais informativos.</p>
+    <div class="buttons">
+    <a class="btn btn-primary" href="login.php" role="button">Cadastre-se</a>
+  </div>
+</div>
+<br>
+<br>
+<footer>
+<div class='rodape'><nav>
+  <div class="div1">
+      <p><strong>Nutri</strong>Ação</p>
+    </div>
+    <div class="div2">
+            <a href="#">Termos de Serviço |</a>
+            <a href="#">Política de Privacidade</a>    
+  </div>
+    <div class="div3"><p>
+  <ion-icon name="logo-youtube"></ion-icon>
+  <ion-icon name="logo-instagram"></ion-icon>
+  <ion-icon name="logo-facebook"></ion-icon></p>
+  </div>
+  </nav>
+</div>
+<div class="rodape2">
+    <P>Feito por <i>GPROGM</i></P>
+  </div>
+  </footer>
+<script  type = "module"  src = "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"> </script> 
+<script  nomodule  src = "https://unpkg .com/ionicons@7.1.0/dist/ionicons/ionicons.js"> </script>
+</body>
+</html>
